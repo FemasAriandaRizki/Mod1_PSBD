fcc0d9e6f6d45b3a11e613e6983cbb61df80bf30
FemasAriandaRizki <human.earth1606@gmail.com>
2025-02-23 00:15:05 +0700

    final project - modul

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+
+Route::get('add', [AdminController::class, 'create'])->name('admin.create');
+
+Route::post('store', [AdminController::class, 'store'])->name('admin.store');
+
+Route::get('edit/{id}', [AdminController::class, 'edit'])->name('admin.edit');
+
+Route::post('update/{id}', [AdminController::class, 'update'])->name('admin.update');
+
+Route::post('delete/{id}', [AdminController::class, 'delete'])->name('admin.delete');
